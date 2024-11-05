57f8bb81ed7d66cfb2e6244794ee964782898f83
WallyDev <wallace07abreu@gmail.com>
2024-11-05 16:59:29 -0300

    eu te amo

--- main.py
+++ main.py
@@ -1,56 +1,3 @@
-# import cv2
-# import mediapipe as mp
-
-# video = cv2.VideoCapture(1)
-
-# hands = mp.solutions.hands
-# Hands = hands.Hands(max_num_hands=1)
-# mpDwaw = mp.solutions.drawing_utils
-
-# while True:
-#     success, img = video.read(0)
-#     frameRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     results = Hands.process(frameRGB)
-#     handPoints = results.multi_hand_landmarks
-#     h, w, _ = img.shape
-#     pontos = []
-#     if handPoints:
-#         for points in handPoints:
-#             mpDwaw.draw_landmarks(img, points,hands.HAND_CONNECTIONS)
-#             #podemos enumerar esses pontos da seguinte forma
-#             for id, cord in enumerate(points.landmark):
-#                 cx, cy = int(cord.x * w), int(cord.y * h)
-#                 #cv2.putText(img, str(id), (cx, cy + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-#                 pontos.append((cx,cy))
-
-#             dedos = [8,12,16,20]
-#             contador = 0
-#             if pontos:
-#                 if pontos[4][0] < pontos[3][0]:
-#                     contador += 1
-#                 for x in dedos:
-#                    if pontos[x][1] < pontos[x-2][1]:
-#                        contador +=1
-
-#             cv2.rectangle(img, (80, 10), (200,110), (255, 0, 0), -1)
-#             cv2.putText(img,str(contador),(100,100),cv2.FONT_HERSHEY_SIMPLEX,4,(255,255,255),5)
-#             #print(contador)
-
-#     cv2.imshow('Imagem',img)
-#     cv2.waitKey(1)
-
-
-
-#     cv2.circle(img,(cx,cy),15,(0,255,0),cv2.FILLED)
-
-
-#     # for id,cord in enumerate(points.landmark):
-#     #     cx, cy = int(cord.x * w), int(cord.y * h)
-#     #     cv2.putText(img, str(id), (cx, cy + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-#     #     pontos.append([cx,cy])
-#     #     if pontos:
-#     #         print(pontos)
-
 import cv2
 import mediapipe as mp
 
@@ -71,10 +18,9 @@ while cap.isOpened():
     if not ret:
         continue
     
-    # Convert the frame to RGB format
+
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
-    # Process the frame to detect hands
     results = Hands.process(frame_rgb)
     handPoints = results.multi_hand_landmarks
     h, w, _ = frame.shape
@@ -96,26 +42,18 @@ while cap.isOpened():
             print(pontos)
             amostradinho = True
             if pontos:
-                if pontos[4][0] < pontos[3][0]:
-                    contador += 1
-                for x in dedos:
-                   if pontos[x][1] < pontos[x-2][1]:
-                       contador +=1
+    # Polegar levantado
+                polegar_levantado = pontos[4][0] > pontos[3][0]
+                # Indicador levantado
+                indicador_levantado = pontos[8][1] < pontos[6][1]
+                # Médio abaixado
+                medio_abaixado = pontos[12][1] > pontos[10][1]
+                # Anelar abaixado
+                anelar_abaixado = pontos[16][1] > pontos[14][1]
+                # Mínimo levantado
+                minimo_levantado = pontos[20][1] < pontos[18][1]
 
-                for x in dedos:
-                    if x == 12:
-                        if pontos[x][1] > pontos[x-2][1]:
-                            amostradinho = False                        
-                        continue
-                    if pontos[x][1] < pontos[x-2][1]:
-                        amostradinho = False
-
-            # cv2.rectangle(frame, (80, 10), (200,110), (255, 0, 0), -1)
-            # cv2.putText(frame,str(contador),(100,100),cv2.FONT_HERSHEY_SIMPLEX,4,(255,255,255),5)
-            
-            if amostradinho:
-                #           ONDE            TEXTO           COORDENADA           FONTE             TAMANHO      COR         GROSSURA
-                cv2.putText(frame,str("TA AMOSTRADINHO"),   (100,100),   cv2.FONT_HERSHEY_SIMPLEX,    1,    (155,25,255),      3)
-
+    if polegar_levantado and indicador_levantado and medio_abaixado and anelar_abaixado and minimo_levantado:
+        cv2.putText(frame, "EU TE AMO", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (155, 25, 255), 3)
     cv2.imshow('Imagem',frame)
     cv2.waitKey(1)
